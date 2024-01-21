681d0ca426c353880d5d9118ff7f88e33f0ca3e6
Rehan Ahmad <147439049+rehan1360@users.noreply.github.com>
2024-01-21 15:12:16 +0530

    Object detection project

---
+++ pp/objectdetect.py
@@ -0,0 +1,29 @@
+import cv2
+import numpy as np
+from ultralytics import YOLO
+# loding model
+model = YOLO('yolov8n.pt')
+
+# loading video
+
+
+# read frmaes
+cap = cv2.VideoCapture(0)
+ret = True
+while ret:
+    ret,frame = cap.read()
+    if ret==True:
+        # detecting objects from frame
+        # track object
+        result = model.track(frame, persist=True)
+
+        #plot results
+        frame_ = result[0].plot()
+
+        # visulaize
+        cv2.imshow('frame',frame_)
+        if cv2.waitKey(1)==ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
